d4ad3e3a62bd895db52375a1c4408abb2429fc3b
Pavel910 <pavel@webiny.com>
2016-09-13 19:48:20 +0200

    Removed remains of old dependencies

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #! /usr/local/bin/node
 
 // Require packages
-var Q = require('q');
 var utils = require('./lib/utils');
 var check = require('./lib/actions/checkRequirements');
 var setup = require('./lib/actions/setup');
@@ -13,35 +12,36 @@ module.exports = {
         utils.log('-----------------------');
         utils.info('Webiny CLI ' + utils.chalk.cyan('v' + version));
         utils.log('-----------------------');
-        if (check.firstRun()) {
-            utils.log('Checking requirements...');
-            return Q.fcall(check.requirements).then(function () {
-                utils.success("Great, all the requirements are in order!");
-                utils.log("\nSetting up the platform...");
-                setup(function (answers) {
-                    utils.log('-------------------------------------');
-                    utils.success('Platform setup is now completed!');
-                    utils.log('-------------------------------------');
-                    utils.info('\nRunning your first development build to get you ready for development...');
-                    // Run first build
-                    var runner = require('./lib/runner');
-                    var config = require('./lib/gulp/config');
-                    var webiny = require('./lib/webiny')(config);
-
-                    config.production = process.env.production = false;
-                    config.esLint = true;
-                    config.buildDir = utils.projectRoot('public_html/build/development');
-                    config.apps = webiny.getApps();
-                    runner('watch', config).then(function () {
-                        utils.success('\nYou are ready to develop! Navigate to the following URL using your favorite browser: ' + utils.chalk.cyan(answers.domain + '/admin'));
-                    });
-
-                });
-            }).fail(function (err) {
-                utils.exclamation(err.message);
-            });
+        if (!check.firstRun()) {
+            return menu();
         }
 
-        menu();
+        // First run will check the system requirements and setup the platform
+        try {
+            utils.log('Checking requirements...');
+            check.requirements();
+            utils.success("Great, all the requirements are in order!");
+            utils.log("\nSetting up the platform...");
+            setup(function (answers) {
+                utils.log('-------------------------------------');
+                utils.success('Platform setup is now completed!');
+                utils.log('-------------------------------------');
+                utils.info('\nRunning your first development build to get you ready for development...');
+                // Run first build
+                var runner = require('./lib/runner');
+                var config = require('./lib/gulp/config');
+                var webiny = require('./lib/webiny')(config);
+
+                config.production = process.env.production = false;
+                config.esLint = true;
+                config.buildDir = utils.projectRoot('public_html/build/development');
+                config.apps = webiny.getApps();
+                runner('watch', config).then(function () {
+                    utils.success('\nYou are ready to develop! Navigate to the following URL using your favorite browser: ' + utils.chalk.cyan(answers.domain + '/admin'));
+                });
+            });
+        } catch (err) {
+            utils.exclamation(err.message);
+        }
     }
 };
