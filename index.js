cfa34668e9b04bdfb0c6b1036a80b917adc873ba
Pavel910 <pavel@webiny.com>
2016-09-14 18:19:00 +0200

    Updated assets parsing to make use of glob patterns. Added check for CLI tool updates.

--- index.js
+++ index.js
@@ -2,46 +2,49 @@
 
 // Require packages
 var utils = require('./lib/utils');
-var check = require('./lib/actions/checkRequirements');
+var checkRequirements = require('./lib/actions/checkRequirements');
+var checkUpdates = require('./lib/actions/checkUpdates');
 var setup = require('./lib/actions/setup');
 var menu = require('./lib/menu');
 var version = JSON.parse(utils.readFile(__dirname + '/package.json')).version;
 
 module.exports = {
     run: function () {
-        utils.log('-----------------------');
-        utils.info('Webiny CLI ' + utils.chalk.cyan('v' + version));
-        utils.log('-----------------------');
-        if (!check.firstRun()) {
-            return menu();
-        }
+        checkUpdates(version).then(function(){
+            utils.log('---------------------------------------------');
+            utils.info('Webiny CLI ' + utils.chalk.cyan('v' + version));
+            utils.log('---------------------------------------------');
+            if (!checkRequirements.firstRun()) {
+                return menu();
+            }
 
-        // First run will check the system requirements and setup the platform
-        try {
-            utils.log('Checking requirements...');
-            check.requirements();
-            utils.success("Great, all the requirements are in order!");
-            utils.log("\nSetting up the platform...");
-            setup(function (answers) {
-                utils.log('-------------------------------------');
-                utils.success('Platform setup is now completed!');
-                utils.log('-------------------------------------');
-                utils.info('\nRunning your first development build to get you ready for development...');
-                // Run first build
-                var runner = require('./lib/runner');
-                var config = require('./lib/gulp/config');
-                var webiny = require('./lib/webiny')(config);
+            // First run will check the system requirements and setup the platform
+            try {
+                utils.log('Checking requirements...');
+                checkRequirements.requirements();
+                utils.success("Great, all the requirements are in order!");
+                utils.log("\nSetting up the platform...");
+                setup(function (answers) {
+                    utils.log('-------------------------------------');
+                    utils.success('Platform setup is now completed!');
+                    utils.log('-------------------------------------');
+                    utils.info('\nRunning your first development build to get you ready for development...');
+                    // Run first build
+                    var runner = require('./lib/runner');
+                    var config = require('./lib/gulp/config');
+                    var webiny = require('./lib/webiny')(config);
 
-                config.production = process.env.production = false;
-                config.esLint = true;
-                config.buildDir = utils.projectRoot('public_html/build/development');
-                config.apps = webiny.getApps();
-                runner('watch', config).then(function () {
-                    utils.success('\nYou are ready to develop! Navigate to the following URL using your favorite browser: ' + utils.chalk.cyan(answers.domain + '/admin'));
+                    config.production = process.env.production = false;
+                    config.esLint = true;
+                    config.buildDir = utils.projectRoot('public_html/build/development');
+                    config.apps = webiny.getApps();
+                    runner('watch', config).then(function () {
+                        utils.success('\nYou are ready to develop! Navigate to the following URL using your favorite browser: ' + utils.chalk.cyan(answers.domain + '/admin'));
+                    });
                 });
-            });
-        } catch (err) {
-            utils.exclamation(err.message);
-        }
+            } catch (err) {
+                utils.exclamation(err.message);
+            }
+        });
     }
 };
