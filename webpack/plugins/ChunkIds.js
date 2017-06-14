e39db978602a058ff38e4831fb0c37c6e1f29f68
Pavel910 <pavel@webiny.com>
2017-06-14 14:50:15 +0200

    Updated ChunkIds plugin to generate chunk id based on containing module paths

--- webpack/plugins/ChunkIds.js
+++ webpack/plugins/ChunkIds.js
@@ -1,3 +1,5 @@
+var crypto = require('crypto');
+
 class ChunkIds {
     apply(compiler) {
         compiler.plugin("compilation", (compilation) => {
@@ -5,24 +7,33 @@ class ChunkIds {
                 chunks.forEach((chunk, index) => {
                     if (!chunk.hasEntryModule() && chunk.id === null) {
                         let id = index;
-                        const context = chunk.modules[0].context;
-                        if (context.includes('/node_modules/')) {
-                            id = context.split('/node_modules/')[1].split('/')[0];
-                        } else if (context.includes('/Ui/Components/')) {
-                            id = context.split('/Ui/Components/')[1].split('/')[0];
-                        } else if (context.includes('/Vendors/')) {
-                            id = 'Vendors-' + context.split('/Vendors/')[1].split('/')[0];
+                        if (process.env.NODE_ENV === 'production') {
+                            chunk.id = compiler.options.name + '-' + this.createChunkIdHash(chunk);
                         } else {
-                            id = context.split('/').pop();
+                            const context = chunk.modules[0].context;
+                            if (context.includes('/node_modules/')) {
+                                id = context.split('/node_modules/')[1].split('/')[0];
+                            } else if (context.includes('/Ui/Components/')) {
+                                id = context.split('/Ui/Components/')[1].split('/')[0];
+                            } else if (context.includes('/Vendors/')) {
+                                id = 'Vendors-' + context.split('/Vendors/')[1].split('/')[0];
+                            } else {
+                                id = context.split('/').pop();
+                            }
+                            // ID must contain the name of the app to avoid ID clashes between multiple apps
+                            chunk.id = compiler.options.name + '-' + index;
+                            // Name is only used in development for easier debugging
+                            chunk.name = id + '-' + index;
                         }
-                        // ID must contain the name of the app to avoid ID clashes between multiple apps
-                        chunk.id = compiler.options.name + '-' + index;
-                        // Name is only used in development for easier debugging
-                        chunk.name = id + '-' + index;
                     }
                 });
             });
         });
+    }
+
+    createChunkIdHash(chunk) {
+        const paths = chunk.modules.map(m => m.context).sort().join('\n');
+        return crypto.createHash('md5').update(paths).digest('hex').substr(0, 10);
     }
 }
 
