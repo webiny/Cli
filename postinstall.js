6b97928a98b92a851f73f10c449318ec248659c1
Pavel910 <pavel@webiny.com>
2017-06-26 19:51:26 +0200

    Updated OS-dependent `profile` file path

--- postinstall.js
+++ postinstall.js
@@ -3,18 +3,24 @@ const username = require('username');
 const chalk = require('chalk');
 const binDir = './node_modules/.bin';
 const Webiny = require('./lib/webiny');
-const profilePath = `/home/${username.sync()}/.profile`;
+const profilePath = process.platform === 'darwin' ? `/Users/${username.sync()}/profile` : `/home/${username.sync()}/.profile`;
 const {magenta} = chalk;
 
-let config = Webiny.readFile(profilePath);
+try {
+    let config = Webiny.readFile(profilePath);
 
-if (config.match(binDir)) {
-    Webiny.info(`${magenta(profilePath)} already exports ${magenta(binDir)} to PATH.`);
-    return;
+    if (config.match(binDir)) {
+        Webiny.info(`${magenta(profilePath)} already exports ${magenta(binDir)} to PATH.`);
+        return;
+    }
+
+    config += `# Added by webiny\nexport PATH="$PATH:${binDir}"`;
+    Webiny.writeFile(profilePath, config);
+    Webiny.success(`Added ${magenta('./node_modules/.bin')} executables to PATH in ${magenta(profilePath)}`);
+    Webiny.info('If not using Webiny vagrant box, please restart your shell or execute the export command yourself as follows:');
+    Webiny.info(magenta('export PATH="$PATH:./node_modules/.bin"'));
+} catch (err) {
+    Webiny.failure('Could not export PATH!');
+    Webiny.log(err);
 }
 
-config += `# Added by webiny\nexport PATH="$PATH:${binDir}"`;
-Webiny.writeFile(profilePath, config);
-Webiny.success(`Added ${magenta('./node_modules/.bin')} executables to PATH in ${magenta(profilePath)}`);
-Webiny.info('If not using Webiny vagrant box, please restart your shell or execute the export command yourself as follows:');
-Webiny.info(magenta('export PATH="$PATH:./node_modules/.bin"'));
