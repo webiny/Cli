b4e0bc0f38c8ed7beb5fc88bc0c0bbb76fa3ad2c
Pavel910 <pavel@webiny.com>
2017-10-02 10:39:30 +0200

    Updated to work on windows. Check for updates is moved to the background and new version notification is printed on process exit.

--- bin/webiny-cli.js
+++ bin/webiny-cli.js
@@ -1,7 +1,3 @@
 #! /usr/bin/env node
-if (!process.env.PWD) {
-    process.env.PWD = __dirname;
-}
-
 const WebinyCli = require('..');
 new WebinyCli().run();
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const program = require('commander');
 const _ = require('lodash');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
+const readline = require('readline');
+
 const checkUpdates = require('./lib/boot/checkUpdates');
 const setup = require('./lib/boot/setup');
 const Menu = require('./lib/navigation');
@@ -10,6 +12,7 @@ const Webiny = require('./lib/webiny');
 
 class WebinyCli {
     constructor() {
+        this.updateAvailable = null;
         this.version = JSON.parse(Webiny.readFile(__dirname + '/package.json')).version;
 
         program
@@ -19,23 +22,35 @@ class WebinyCli {
                 'It supports plugins so you are welcome to develop new plugins for your project and connect to the existing plugins using hooks.',
                 'Run without arguments to enter GUI mode.',
                 '',
-                'Visit https://www.webiny.com/ for tutorials and documentarion.'
+                'Visit https://www.webiny.com/ for tutorials and documentation.'
             ].join('\n  '))
             .option('--show-timestamps [format]', 'Show timestamps next to each console message')
             .option('--env [env]', 'Setup project using specified environment (default or docker)');
-
-        const stdin = process.stdin;
-        stdin.setRawMode(true);
-        stdin.resume();
-        process.stdin.write('Loading plugins...');
-        Webiny.getPlugins();
-        process.stdout.clearLine();
-        process.stdout.cursorTo(0);
 
         program.command('*', null, {noHelp: true}).action(cmd => {
             Webiny.failure(`${chalk.magenta(cmd)} is not a valid command. Run ${chalk.magenta('webiny-cli -h')} to see all available commands.`);
             process.exit(1);
         });
+
+        this.attachExitHandler();
+
+        // Load plugins
+        readline.cursorTo(process.stdout, 0);
+        process.stdout.write('Loading plugins...');
+        Webiny.getPlugins();
+        readline.clearLine(process.stdout, 0);
+        readline.cursorTo(process.stdout, 0, null);   
+    }
+
+    exit() {
+        if(this.updateAvailable) {
+            const {currentVersion, latestVersion} = this.updateAvailable;
+            const line = '---------------------------------------------';
+            Webiny.log('\n' + chalk.green(line));
+            Webiny.success('Update available: ' + chalk.green(latestVersion) + chalk.grey(' (current: ' + currentVersion + ')'));
+            Webiny.info('Run ' + chalk.blue('yarn add webiny-cli@' + latestVersion) + ' to update');
+            Webiny.log(chalk.green(line) + '\n');
+        }
     }
 
     run() {
@@ -45,55 +60,74 @@ class WebinyCli {
             require('console-stamp')(console, _.isString(program.showTimestamps) ? program.showTimestamps : 'HH:MM:ss.l');
         }
 
+        checkUpdates(this.version).then(update => this.updateAvailable = update);
+
         if (program.args.length === 0) {
-            checkUpdates(this.version).then(() => {
-                Webiny.log('---------------------------------------------');
-                Webiny.info('Webiny CLI ' + chalk.cyan('v' + this.version));
-                Webiny.log('---------------------------------------------');
-                const checkRequirements = require('./lib/boot/checkRequirements');
-                if (!checkRequirements.firstRun()) {
-                    const menu = new Menu();
-                    return menu.render();
-                }
+            Webiny.log('---------------------------------------------');
+            Webiny.info('Webiny CLI ' + chalk.cyan('v' + this.version));
+            Webiny.log('---------------------------------------------');
+            const checkRequirements = require('./lib/boot/checkRequirements');
+            if (!checkRequirements.firstRun()) {
+                const menu = new Menu();
+                return menu.render();
+            }
 
-                return inquirer.prompt([{
-                    type: 'list',
-                    name: 'env',
-                    when: !program.env,
-                    message: 'Select your development environment',
-                    choices: [
-                        {name: 'Vagrant (or plain Linux/Mac)', value: 'default'},
-                        {name: 'Docker', value: 'docker'}
-                    ]
-                }]).then(answers => {
-                    const selectedEnv = answers.env || program.env;
+            return inquirer.prompt([{
+                type: 'list',
+                name: 'env',
+                when: !program.env,
+                message: 'Select your development environment',
+                choices: [
+                    {name: 'Vagrant (or plain Linux/Mac)', value: 'default'},
+                    {name: 'Docker', value: 'docker'}
+                ]
+            }]).then(answers => {
+                const selectedEnv = answers.env || program.env;
 
-                    try {
-                        if (selectedEnv !== 'docker') {
-                            // First run will check the system requirements and setup the platform
-                            Webiny.log('Checking requirements...');
-                            checkRequirements.requirements();
-                            Webiny.success("Great, all the requirements are in order!");
-                        }
-
-                        Webiny.log("\nSetting up the platform...");
-                        return setup(selectedEnv).then(answers => {
-                            Webiny.log(`\n-------------------------------------`);
-                            Webiny.success('Platform setup is now completed!');
-                            Webiny.info(`You are now ready to run your first development build! Select "Develop!" from the menu and hit ENTER.\nAfter the development build is completed, navigate to ` + chalk.magenta(answers.domain + '/admin') + ` to see your brand new administration system!`);
-                            Webiny.log('-------------------------------------');
-                            const menu = new Menu();
-                            menu.render();
-                        }).catch(e => {
-                            Webiny.failure(e.message, e);
-                        });
-                    } catch (err) {
-                        Webiny.exclamation('Setup failed with the following problem:', err);
-                        process.exit(1);
+                try {
+                    if (selectedEnv !== 'docker') {
+                        // First run will check the system requirements and setup the platform
+                        Webiny.log('Checking requirements...');
+                        checkRequirements.requirements();
+                        Webiny.success("Great, all the requirements are in order!");
                     }
-                });
+
+                    Webiny.log("\nSetting up the platform...");
+                    return setup(selectedEnv).then(answers => {
+                        Webiny.log(`\n-------------------------------------`);
+                        Webiny.success('Platform setup is now completed!');
+                        Webiny.info(`You are now ready to run your first development build! Select "Develop!" from the menu and hit ENTER.\nAfter the development build is completed, navigate to ` + chalk.magenta(answers.domain + '/admin') + ` to see your brand new administration system!`);
+                        Webiny.log('-------------------------------------');
+                        const menu = new Menu();
+                        menu.render();
+                    }).catch(e => {
+                        Webiny.failure(e.message, e);
+                    });
+                } catch (err) {
+                    Webiny.exclamation('Setup failed with the following problem:', err);
+                    process.exit(1);
+                }
             });
         }
+    }
+
+    attachExitHandler(){
+        if (process.platform === "win32") {
+            const rl = require("readline").createInterface({
+              input: process.stdin,
+              output: process.stdout
+            });
+          
+            rl.on("SIGINT", () => {
+              process.emit("SIGINT");
+            });
+          }
+
+        // Listen for process exit
+        process.on('exit', this.exit.bind(this));
+
+        // Ctrl+C event
+        process.on('SIGINT', this.exit.bind(this));
     }
 }
 
