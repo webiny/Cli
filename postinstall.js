ceb2636cb361e58a6e9225d51c56a95f140a94d9
Pavel910 <pavel@webiny.com>
2017-10-03 13:07:10 +0200

    Better handling of export to PATH.

--- postinstall.js
+++ postinstall.js
@@ -1,29 +1,38 @@
-const username = require('username');
+const binDir = './node_modules/.bin';
+
+if (process.platform === "win32") {
+    console.log(`Please add "${binDir}" to Windows $PATH for webiny-cli to be available in cmd prompt!`);
+    process.exit(0);
+}
+
 const chalk = require('chalk');
 const Webiny = require('./lib/webiny');
 const {magenta} = chalk;
-const binDir = './node_modules/.bin';
-const OSX = process.platform === 'darwin';
 
 try {
-    const user = username.sync();
-    const osxBashProfile = `/Users/${user}/.bash_profile`;
-    let profilePath = OSX ? `/Users/${user}/.profile` : `/home/${user}/.profile`;
-    if (OSX && Webiny.fileExists(osxBashProfile)) {
-        profilePath = osxBashProfile;
+    const userInfo = require('os').userInfo();
+    const bashProfile = `${userInfo.homedir}/.bash_profile`;
+    let exportTo = `${userInfo.homedir}/.profile`;
+
+    if (Webiny.fileExists(bashProfile)) {
+        exportTo = bashProfile;
     }
 
-    let config = Webiny.readFile(profilePath);
+    if (!Webiny.fileExists(exportTo)) {
+        Webiny.writeFile(exportTo, '');
+    }
+
+    let config = Webiny.readFile(exportTo);
 
     if (config.match(binDir)) {
-        Webiny.info(`${magenta(profilePath)} already exports ${magenta(binDir)} to PATH.`);
+        Webiny.info(`${magenta(exportTo)} already exports ${magenta(binDir)} to PATH.`);
         return;
     }
 
     config += `# Added by webiny-cli\nexport PATH="$PATH:${binDir}"`;
-    Webiny.writeFile(profilePath, config);
-    Webiny.success(`Added ${magenta('./node_modules/.bin')} executables to PATH in ${magenta(profilePath)}`);
-    Webiny.info('If not using Webiny vagrant box, please restart your shell or execute the export command yourself as follows:');
+    Webiny.writeFile(exportTo, config);
+    Webiny.success(`Added ${magenta('./node_modules/.bin')} executables to PATH in ${magenta(exportTo)}`);
+    Webiny.info('If not using Webiny Vagrant box, please restart your session or execute the export command yourself as follows:');
     Webiny.info(magenta('export PATH="$PATH:./node_modules/.bin"'));
 } catch (err) {
     Webiny.failure('Could not export PATH!');
